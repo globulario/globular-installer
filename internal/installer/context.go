992171d13b1d58501d5c1c96d782de902027fe10
davecourtois <dave.courtois60@gmail.com>
2025-12-31 09:59:27 -0500

    implement options

--- internal/installer/context.go
+++ internal/installer/context.go
@@ -1,11 +1,97 @@
 package installer
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/globulario/globular-installer/internal/installer/manifest"
+	"github.com/globulario/globular-installer/internal/platform"
+)
+
+const (
+	DefaultPrefix    = "/usr/lib/globular"
+	DefaultStateDir  = "/var/lib/globular"
+	DefaultConfigDir = "/etc/globular"
+)
+
 type Context struct {
-	Logger Logger
-	DryRun bool
+	Version        string
+	Prefix         string
+	StateDir       string
+	ConfigDir      string
+	Features       FeatureSet
+	NonInteractive bool
+	DryRun         bool
+	Logger         Logger
+	Platform       platform.Platform
+	Manifest       *manifest.Manifest
+	ManifestPath   string
 }
 
 func NewContext(opts Options) (*Context, error) {
-	_ = opts
-	return &Context{}, nil
+	opts = opts.Normalized()
+
+	prefix := opts.Prefix
+	if prefix == "" {
+		prefix = DefaultPrefix
+	}
+	stateDir := opts.StateDir
+	if stateDir == "" {
+		stateDir = DefaultStateDir
+	}
+	configDir := opts.ConfigDir
+	if configDir == "" {
+		configDir = DefaultConfigDir
+	}
+
+	for name, value := range map[string]string{
+		"prefix":    prefix,
+		"stateDir":  stateDir,
+		"configDir": configDir,
+	} {
+		if !filepath.IsAbs(value) {
+			return nil, fmt.Errorf("%s %q must be absolute", name, value)
+		}
+	}
+
+	logger := NewStdLogger(opts.Verbose)
+	features := ParseFeatures(opts.FeaturesCSV)
+
+	plat, err := platform.Detect()
+	if err != nil {
+		return nil, fmt.Errorf("detect platform: %w", err)
+	}
+
+	mpath := manifest.DefaultPath(stateDir)
+	m, err := manifest.Load(mpath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			m = manifest.New(opts.Version, prefix)
+		} else {
+			return nil, fmt.Errorf("load manifest %s: %w", mpath, err)
+		}
+	}
+
+	ctx := &Context{
+		Version:        opts.Version,
+		Prefix:         prefix,
+		StateDir:       stateDir,
+		ConfigDir:      configDir,
+		Features:       features,
+		NonInteractive: opts.NonInteractive,
+		DryRun:         opts.DryRun,
+		Logger:         logger,
+		Platform:       plat,
+		Manifest:       m,
+		ManifestPath:   mpath,
+	}
+
+	if logger != nil {
+		logger.Infof("context: version=%q prefix=%q stateDir=%q configDir=%q dryRun=%v nonInteractive=%v",
+			ctx.Version, ctx.Prefix, ctx.StateDir, ctx.ConfigDir, ctx.DryRun, ctx.NonInteractive)
+	}
+
+	return ctx, nil
 }
--- internal/installer/options.go
+++ internal/installer/options.go
@@ -1,3 +1,23 @@
 package installer
 
-type Options struct{}
+import "strings"
+
+type Options struct {
+	Version        string
+	Prefix         string
+	StateDir       string
+	ConfigDir      string
+	FeaturesCSV    string
+	NonInteractive bool
+	DryRun         bool
+	Verbose        bool
+}
+
+func (o Options) Normalized() Options {
+	o.Version = strings.TrimSpace(o.Version)
+	o.Prefix = strings.TrimSpace(o.Prefix)
+	o.StateDir = strings.TrimSpace(o.StateDir)
+	o.ConfigDir = strings.TrimSpace(o.ConfigDir)
+	o.FeaturesCSV = strings.TrimSpace(o.FeaturesCSV)
+	return o
+}
